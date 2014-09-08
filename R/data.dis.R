404dd426762f8e79555c1c405a61a9d6c65efa3c
Jay Ulfelder <ulfelder@gmail.com>
2014-09-08 16:02:01 -0400

    Update data.dis.R
    Remove file paths specific to my hard drive.

--- R/data.dis.R
+++ R/data.dis.R
@@ -8,20 +8,21 @@ rm(list=ls(all=TRUE))
 
 # Load required packages and functions
 library(XLConnect)
-source("c:/users/jay/documents/ushmm/statrisk.replication/r/f.pitfcodeit.r")
-source("c:/users/jay/documents/ushmm/statrisk.replication/r/f.countryyearrackit.r")
+source("r/f.pitfcodeit.r")
+source("r/f.countryyearrackit.r")
 
 # Load the data
-dis <- readWorksheetFromFile("c:/users/jay/documents/ushmm/statrisk.replication/data.in/diss2013.xls", sheet=1)
+dis <- readWorksheetFromFile("data.in/diss2013.xls", sheet=1)
 
 # Append prefix to names
 names(dis) <- c("sftgcode", "year", paste0("dis.", names(dis)[3:length(names(dis))]))
 
 # Merge with complete rack of country-years to get complete set (original doesn't include countries w/all 0s)
-rack <- merge(subset(pitfcodeit(countryyearrackit(min(dis$year), max(dis$year)), "country"), select = c(sftgcode, year)), dis, all.x = TRUE)
+rack <- merge(subset(pitfcodeit(countryyearrackit(min(dis$year), max(dis$year)), "country"),
+  select = c(sftgcode, year)), dis, all.x = TRUE)
 
 # Fill in NAs with 0s so variables don't get wiped out in transformation process
 rack <- replace(rack, is.na(rack)==TRUE, 0)
 
 # Write it out
-write.csv(dis, "c:/users/jay/documents/ushmm/statrisk.replication/data.out/dis.csv", row.names=FALSE)
+write.csv(dis, "data.out/dis.csv", row.names=FALSE)
