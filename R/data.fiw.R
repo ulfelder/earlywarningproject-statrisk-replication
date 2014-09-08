06029a2cacdbbb555c8471954d0e81be702913a8
Jay Ulfelder <ulfelder@gmail.com>
2014-09-08 16:06:52 -0400

    Update data.fiw.R
    Removed file paths specific to my hard drive, added some notes.

--- R/data.fiw.R
+++ R/data.fiw.R
@@ -10,11 +10,11 @@ rm(list=ls(all=TRUE))
 # Load requisite packages and functions
 require(xlsx)
 require(reshape)
-source("c:/users/jay/documents/ushmm/statrisk.replication/r/f.pitfcodeit.r")
-source("c:/users/jay/documents/ushmm/statrisk.replication/r/f.countryyearrackit.r")
+source("r/f.pitfcodeit.r")
+source("r/f.countryyearrackit.r")
 
 # Ingest and reshape data into desired country-year format
-fiw <- read.xlsx2("c:/users/jay/documents/ushmm/statrisk.replication/data.in/Country Ratings and Status, 1973-2014 (FINAL).xls",
+fiw <- read.xlsx2("data.in/Country Ratings and Status, 1973-2014 (FINAL).xls",
   sheetIndex = 1, startRow = 7, endRow = 212)
 fhyrs <- c(1972:1980,1982:2013)
 var_years <- expand.grid( x=c('PR', 'CL', 'Status'), y = fhyrs)
@@ -39,9 +39,11 @@ fiw$fiw.cl <- as.numeric(as.character(fiw$fiw.cl))
 fiw$fiw.status <- as.character(fiw$fiw.status)
 fiw$fiw.status[fiw$fiw.status==".."] <- fiw$fiw.status[fiw$fiw.status=="F (NF)"] <- NA
 
+# Add PITF country codes for merging and get rid of country names
 fiw <- pitfcodeit(fiw, "country")
 fiw$country <- NULL
 
+# Delete missing values
 fiw <- na.omit(fiw)
 
-write.csv(fiw, "c:/users/jay/documents/ushmm/statrisk.replication/data.out/fiw.csv", row.names = FALSE)
+write.csv(fiw, "data.out/fiw.csv", row.names = FALSE)
